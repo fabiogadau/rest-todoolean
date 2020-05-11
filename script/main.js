06518c0c54661c1a094f3298182b63058fdaf6bf
Fabio Gadau <fabiogadau92@gmail.com>
2020-05-11 13:43:02 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,13 @@
+/*
+* TODOOLEAN
+- Creazione di una todo list con le seguenti funzionalità, attraverso l’uso delle API, AJAX, jQuery e Handlebars
+- Lettura di tutti i todo
+- Creazione nuovo todo
+- Cancellazione todo
+*/
+
+$(document).ready(function() {
+
+  
+
+}); // End document ready
