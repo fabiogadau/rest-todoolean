70683fc2a5c7d32a3dacd6e16dd4e7a7c38f0926
Fabio Gadau <fabiogadau92@gmail.com>
2020-05-11 15:33:31 +0200

    js ok

--- script/main.js
+++ script/main.js
@@ -8,6 +8,116 @@
 
 $(document).ready(function() {
 
+  /************************************************************************
+  * References
+  ************************************************************************/
+  // Document references
+  var todoInput = $('#todo-input');
+  var todoBtn = $('#todo-btn');
+  var todoList = $('.todo-list');
+
+  // API
+  var todoAPI = 'http://157.230.17.132:3004/todos';
+
+  // Init Handlebars
+  var source = $('#todo-template').html();
+  var template = Handlebars.compile(source);
+
   
+  /************************************************************************
+  * Actions
+  ************************************************************************/
+  // Get and print all todos
+  printTodos(template, todoAPI, todoList);
+
+  // Create new todo by writing into input and clicking button
+  todoBtn.click(function(){
+    createTodo(template, todoAPI, todoList, todoInput);
+  });
+
+  // Create new todo by writing into input and Enter key keypress
+  $(document).keypress(function(event){
+    if ( event.which == 13 || event.keyCode == 13 ) {
+      createTodo(template, todoAPI, todoList, todoInput);
+    }
+  });
+
+  // Remove todo by clicking its button
+  $(document).on('click', '#remove', function(){
+    deleteTodo(template, todoAPI, todoList, $(this));
+  });
+  
+
+  /************************************************************************* 
+  * FUNCTIONS
+  *************************************************************************/
+  // Function to print todos
+  function printTodos(template, newApi, newParent){
+    // reset
+    newParent.html('');
+
+    // start AJAX call
+    $.ajax({
+      url: newApi,
+      method: 'GET',
+      success: function(result){
+        var object = result;
+
+        for ( var i = 0; i < object.length; i++ ) {
+          var objectItem = object[i];
+
+          var newObject = {
+            todoText: objectItem.text,
+            todoId: objectItem.id
+          };
+
+          var toPrint = template(newObject);
+          newParent.append(toPrint);
+        };
+      },
+      error: function(){
+        console.log('Error occured while printing content');
+      }
+    }); // end AJAX call
+  };
+  
+  // Function to create a new todo
+  function createTodo(template, newApi, newParent, newInput){
+    var newValue = newInput.val().trim().toLowerCase();
+
+    // start AJAX call
+    $.ajax({
+      url: newApi,
+      method: 'POST',
+      data: {
+        text: newValue
+      },
+      success: function(){
+        printTodos(template, newApi, newParent)
+
+        newInput.val('');
+      },
+      error: function(){
+        console.log('Error occured while creating content');
+      }
+    }); // end AJAX call
+  };
+
+  // Function to remove a todo
+  function deleteTodo(template, newApi, newParent, self){
+    var elementId = self.data('id');
+
+    // start AJAX call
+    $.ajax({
+      url: newApi + '/' + elementId,
+      method: 'DELETE',
+      success: function(){
+        printTodos(template, newApi, newParent)
+      },
+      error: function(){
+        console.log('Error occured while deleting content');
+      }
+    });// end AJAX call
+  };
 
 }); // End document ready
